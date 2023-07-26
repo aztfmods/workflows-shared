13b94dfc4ac3c81ab469d38bb32edae6c3b1c555
dkool <dennis.kool@cloudnation.nl>
2023-07-26 16:26:50 +0200

    cleanup

--- tests/global_test.go
+++ tests/global_test.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"net/http"
+	"net/url"
 	"os"
 	"strings"
+	"sync"
 	"testing"
 
 	"mvdan.cc/xurls/v2"
@@ -18,23 +20,37 @@ func TestURLs(t *testing.T) {
 	rxStrict := xurls.Strict()
 	urls := rxStrict.FindAllString(string(data), -1)
 
-	for _, url := range urls {
+	var wg sync.WaitGroup
+	for _, u := range urls {
 		// Skip Terraform Registry URLs because of status 200 on non-existing URLs.
-		if strings.Contains(url, "registry.terraform.io/providers/") {
+		if strings.Contains(u, "registry.terraform.io/providers/") {
 			continue
 		}
 
-		resp, err := http.Get(url)
+		// Parse the URL before making a request
+		_, err := url.Parse(u)
 		if err != nil {
-			t.Errorf("Failed: URL: %s, Error: %v", url, err)
 			continue
 		}
-		resp.Body.Close()
 
-		if resp.StatusCode != http.StatusOK {
-			t.Errorf("Failed: URL: %s, Status code: %d", url, resp.StatusCode)
-		} else {
-			t.Logf("Success: URL: %s, Status code: %d", url, resp.StatusCode)
-		}
+		wg.Add(1)
+		go func(url string) {
+			defer wg.Done()
+
+			resp, err := http.Get(url)
+			if err != nil {
+				t.Errorf("Failed: URL: %s, Error: %v", url, err)
+				return
+			}
+			defer resp.Body.Close()
+
+			if resp.StatusCode != http.StatusOK {
+				t.Errorf("Failed: URL: %s, Status code: %d", url, resp.StatusCode)
+			} else {
+				t.Logf("Success: URL: %s, Status code: %d", url, resp.StatusCode)
+			}
+		}(u)
 	}
+	wg.Wait()
 }
+
